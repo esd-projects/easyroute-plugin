b3b48ed5d87cbc25f7795f18e5de2fca6738c05f
zhangjincheng <zhangjincheng@huidr.com>
2019-06-13 16:55:43 +0800

    支持接口的扫描

---
+++ src/EasyRoute/Annotation/MqttMapping.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: administrato
+ * Date: 2019/6/13
+ * Time: 16:54
+ */
+
+namespace ESD\Plugins\EasyRoute\Annotation;
+
+
+class MqttMapping extends RequestMapping
+{
+    public $method = ["mqtt"];
+}
